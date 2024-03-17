6aa34b975b22328e14603eada2b0e12951da5dc0
AlexandreStang <91079041+AlexandreStang@users.noreply.github.com>
2024-03-17 00:47:15 -0400

    Now generate multiple choice answers
    Changed the name of multiple variables, questions now generate multiple choice answers, experimented with bootstrap CSS, and began working on the form interactivity

--- js/script.js
+++ js/script.js
@@ -1,28 +1,56 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    playJeuAlterations();
 
-    // Combien de di`eses y a t-il dans la gamme de r´ e majeur? (4)
-    // La gamme de si mineur a t-elle 2 ou 3 di`eses? (2)
-    // Les alt´ erations de la gamme de fa mineur sont elles des b´emols ou des di` eses (b´ emol)
-
-    // Quelle est la tonalit´e majeure qui contient 3 di`eses? (la),
-    // quelles sont les tonalit´es sans alt´eration `a la cl´e?
 
 });
+
+
+// VARIABLES GLOBALES -----------------------------
+
+
+let timer = 0;
+let score = 0;
+let maxChoixReponse = 4;
 
 
 // MÉTHODES DE JEU -----------------------------
 
 
-function playJeuAlterations() {
+function playJeu() {
     const cycleDesQuintes = getCycleDesQuintes();
+    score = 0;
 
     let index = Math.floor(Math.random() * (cycleDesQuintes.length));
     console.log(index);
 
-    console.log(genQuestionNbSymb(cycleDesQuintes, index));
-    console.log(genQuestionTonalite(cycleDesQuintes, index));
+    let question = genQuestionNbAlterations(cycleDesQuintes, index);
+    console.log(question);
+    let question2 = genQuestionTonalite(cycleDesQuintes, index);
+    console.log(question2);
+
+    $("#jeuContenu").empty().append("<h1>" + question.question + "</h1>");
+
+}
+
+
+function validerReponse() {
+    let reponse = "reponse2";
+    let choix = $('[name="questionnaire"]');
+
+    for (let i = 0; i < choix.length; i++) {
+        if (choix[i].checked) {
+            console.log(choix[i]);
+            if (choix[i].id === reponse) {
+                console.log("You win!!");
+                score++;
+                $("#score").empty().append(score);
+            }
+        }
+    }
+}
+
+
+function startTimer() {
 
 }
 
@@ -31,40 +59,84 @@ function playJeuAlterations() {
 
 
 // Générer une question qui demande à l'utilisateur le nombre de bémols/dièses associés à une gamme (à l'index donné)
-function genQuestionNbSymb(cycleDesQuintes, index) {
+function genQuestionNbAlterations(cycleDesQuintes, index) {
     let quinte = cycleDesQuintes[index];
 
-    // Assembler la question
-    let question = "Combien de " + getNomSymbole(quinte.symbTonalite(), true) + 
-        " y a t-il dans la gamme de " + getRandomMode(quinte) + "?";
-    let reponse = quinte.nbSymbTonalite();
+    if (quinte.nbAlterations() === 0) {
+        return null; // TODO: Write a generic question instead
+    }
 
-    return new Question(question, reponse, null);
+    // Assembler la question: "Combien de (dièses/bémols) y a t-il dans la gamme de (nom de gamme)?"
+    let question = "Combien de " + getNomSymbole(quinte.alteration(), true) +
+        " y a t-il dans la gamme de " + quinte.getRandomMode() + "?";
+    let reponse = quinte.nbAlterations();
+
+    // Générer les choix de réponses
+    let maxNbAlterations = cycleDesQuintes[0].nbAlterations();
+    let choixReponse = [reponse];
+
+    while (choixReponse.length < maxChoixReponse) {
+        let random = Math.floor(Math.random() * maxNbAlterations+1);
+        let isValid = true;
+
+        for (let j = 0; j < choixReponse.length; j++) {
+            if (choixReponse[j] === random) {
+                isValid = false;
+                break;
+            }
+        }
+
+        if (isValid) {
+            choixReponse.push(random);
+        }
+    }
+
+    return new Question(question, reponse, choixReponse);
 }
 
 // Générer une question qui demande à l'utilisateur la tonalité associée à un nombre de bémols/dièses
 function genQuestionTonalite(cycleDesQuintes, index) {
     let quinte = cycleDesQuintes[index];
+    let gamme = quinte.getRandomMode().split(" ");
 
-    let gamme = getRandomMode(quinte);
-    let tabGamme = gamme.split(" ");
-
-    let gammeFeminin = " ";
-
-    switch (tabGamme[1]) {
+    // Obtenir l'écriture au féminin de la tonalitée mineure/majeure pour l'écriture de la question
+    let gammeAuFeminin = "";
+    switch (gamme[1]) {
         case "mineur":
-            gammeFeminin = "mineure";
+            gammeAuFeminin = "mineure";
             break;
         case "majeur":
-            gammeFeminin = "majeure";
+            gammeAuFeminin = "majeure";
             break;
         default:
             return null;
     }
 
-    let reponse = tabGamme[0];
-    let question = "Quelle est la tonalitée " + gammeFeminin + " qui contient " + quinte.tonaliteTexte() + "?"
-    return new Question(question, reponse, null);
+    // Assembler la question: "Quelle est la tonalitée (majeure/mineure) qui contient (int) (dièses/bémols)?"
+    let question = "Quelle est la tonalitée " + gammeAuFeminin + " qui contient " + quinte.tonaliteToTexte() + "?"
+    let reponse = gamme[0];
+
+    // Générer les choix de réponses
+    let choixReponse = [reponse];
+
+    while (choixReponse.length < maxChoixReponse) {
+        let random = Math.floor(Math.random() * cycleDesQuintes.length);
+        let randomReponse = cycleDesQuintes[random].getRandomMode().split(" ")[0];
+        let isValid = true;
+
+        for (let j = 0; j < choixReponse.length; j++) {
+            if (choixReponse[j] === randomReponse) { // TODO: Remove obvious wrong answers
+                isValid = false;
+                break;
+            }
+        }
+
+        if (isValid) {
+            choixReponse.push(randomReponse);
+        }
+    }
+
+    return new Question(question, reponse, choixReponse);
 }
 
 
@@ -85,6 +157,7 @@ function getCycleDesQuintes() {
             cycleDesQuintes.push(new Quinte(tabTonalite[i], tabModeMajeur[i], tabModeMineur[i]));
         }
     }
+
     return cycleDesQuintes;
 }
 
@@ -108,18 +181,6 @@ function getNomSymbole(symbole, isPluriel) {
     }
 }
 
-// Obtenir au hasard la gamme majeure ou la gamme mineure d'une quinte
-function getRandomMode(quinte) {
-    switch(Math.floor(Math.random() * 2)) {
-        case 0:
-            return quinte.modeMineur;
-        case 1:
-            return quinte.modeMajeur;
-        default:
-            return null;
-    }
-}
-
 
 // CLASSES -----------------------------
 
@@ -132,33 +193,49 @@ class Quinte {
         this.modeMineur = modeMineur;
     }
 
-    tonaliteTexte() {
-        let nbSymboles = this.nbSymbTonalite();
+    // Convertir la tonalité sous forme textuelle
+    tonaliteToTexte() {
+        let nbAlterations = this.nbAlterations();
         let isPluriel = false;
 
-        if (nbSymboles > 1) {
+        if (nbAlterations === "0") {
+            return "aucune alteration";
+        } else if (nbAlterations > 1) {
             isPluriel = true;
         }
 
-        return nbSymboles + " " + getNomSymbole(this.symbTonalite(), isPluriel);
+        return nbAlterations + " " + getNomSymbole(this.alteration(), isPluriel);
     }
 
-    symbTonalite() {
+    // Obtenir l'altération présente à l'intérieur de la tonalité (dièse ou bémol)
+    alteration() {
         return this.tonalite.split(" ")[1];
     }
 
-    nbSymbTonalite() {
-        return this.tonalite.split(" ")[0];
+    // Obtenir le nombre d'altérations (dièses/bémols) présentes à l'intérieur de la tonalité
+    nbAlterations() {
+        return Number(this.tonalite.split(" ")[0]);
     }
 
+    // Obtenir au hasard la gamme majeure ou la gamme mineure
+    getRandomMode() {
+        switch(Math.floor(Math.random() * 2)) {
+            case 0:
+                return this.modeMineur;
+            case 1:
+                return this.modeMajeur;
+            default:
+                return null;
+        }
+    }
 }
 
 // Classe représentant une question
 class Question {
-    constructor(question, reponse, mauvaisesReponses) {
+    constructor(question, reponse, choixReponse) {
         this.question = question;
         this.reponse = reponse;
-        this.mauvaisesReponses = mauvaisesReponses;
+        this.choixReponse = choixReponse;
     }
 }
 
